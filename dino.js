9f6d0520660835d956cb64b400e584125a6e7b56
Sandraroarr <12160073@unmsm.edu.pe>
2022-02-14 17:26:58 -0500

    Dino jumping completed

--- dino.js
+++ dino.js
@@ -1,36 +1,64 @@
+import {
+  incrementCustomProperty,
+  setCustomProperty,
+  getCustomProperty,
+} from "./updateCustomProperty.js"
+
 const dinoElem = document.querySelector("[data-dino]")
-const JUMP_SPEED = .45
-const GRAVITY = .011
+const JUMP_SPEED = 0.45
+const GRAVITY = 0.0015
 const DINO_FRAME_COUNT = 2
 const FRAME_TIME = 100
 
 let isJumping
 let dinoFrame
 let currentFrameTime
-export function setupDino(){
+let yVelocity
+export function setupDino() {
   isJumping = false
   dinoFrame = 0
   currentFrameTime = 0
-}
-export function updateDino(delta, speedScale){
-  handleRun(delta, speedScale)
-  handleJump()
+  yVelocity = 0
+  setCustomProperty(dinoElem, "--bottom", 0)
+  document.removeEventListener("keydown", onJump)
+  document.addEventListener("keydown", onJump)
 }
 
-function handleRun(delta, speedScale){
-  if (isJumping){
+export function updateDino(delta, speedScale) {
+  handleRun(delta, speedScale)
+  handleJump(delta)
+}
+
+function handleRun(delta, speedScale) {
+  if (isJumping) {
     dinoElem.src = `imgs/dino-stationary.png`
     return
   }
 
-  if (currentFrameTime >= FRAME_TIME){
+  if (currentFrameTime >= FRAME_TIME) {
     dinoFrame = (dinoFrame + 1) % DINO_FRAME_COUNT
     dinoElem.src = `imgs/dino-run-${dinoFrame}.png`
     currentFrameTime -= FRAME_TIME
   }
-    currentFrameTime += delta * speedScale
+  currentFrameTime += delta * speedScale
 }
 
-function handleJump(){
+function handleJump(delta) {
+  if (!isJumping) return
 
+  incrementCustomProperty(dinoElem, "--bottom", yVelocity * delta)
+
+  if (getCustomProperty(dinoElem, "--bottom") <= 0) {
+    setCustomProperty(dinoElem, "--bottom", 0)
+    isJumping = false
+  }
+
+  yVelocity -= GRAVITY * delta
+}
+
+function onJump(e) {
+  if (e.code !== "Space" || isJumping) return
+
+  yVelocity = JUMP_SPEED
+  isJumping = true
 }
